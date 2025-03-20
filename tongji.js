c575b202b0ae2cce161d51c2137baebf217d873f
99ITDOG <99itdog@gmail.com>
2025-03-20 15:01:51 +0800

    Create tongji.js

---
+++ tongji.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?9257d847d44063644e5b11111e6973fa";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+//百度统计代码
